fc1b36968ea8291d37b69282ca89351e74ff9650
Francjatomarka <84529549+Francjatomarka@users.noreply.github.com>
2021-12-08 16:28:00 +0000

    Create offsets.h

---
+++ offsets.h
@@ -0,0 +1,25 @@
+	#define cl_entitylist  0x1971e08
+	#define LocalPlayer  0x1d21668
+	//#define NAME_INDEX  0x38
+	#define m_iTeamNum  0x448
+	#define m_iHealth  0x438
+	#define m_shieldHealth  0x170
+	#define m_iName  0x589
+	#define m_iSignifierName  0x580
+	#define lastVisibleTime  0x1af4
+	#define NameList  0xba17f40
+	#define m_lifeState  0x798
+	#define m_bleedoutState  0x2718
+	//#define BULLET_SPEED  0x1ED0
+	//#define BULLET_GRAVITY  0x1ED8
+	#define m_latestPrimaryWeapons  0x1a8c
+	#define m_vecAbsOrigin  0x14c
+	#define m_bConstrainBetweenEndpoints  0x0f38
+	#define m_vecPunchWeapon_Angle  0x2480
+	#define camera_origin  0x1f20
+	#define ViewAngles  0x2568 //m_ammoPoolCapacity - 0x14 //work
+	#define BreathAngles (ViewAngles - 0x10)
+	#define ViewMatrix  0x1b3bd0
+	#define ViewRender  0x7511b88
+	#define ItemID  0x16a4 //m_weaponNameIndex //PROPSURVIVAL
+	#define ClientState  0x130c3f0
